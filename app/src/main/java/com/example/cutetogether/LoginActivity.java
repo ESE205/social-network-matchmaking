0d60d49c42a1ae1fe5c23bed8cd4ed4f1b37296c
bradfordorr <orrbradford@gmail.com>
2019-03-04 20:48:20 -0600

    added match and profile activities along with corresponding xml files

--- app/src/main/java/com/example/cutetogether/LoginActivity.java
+++ app/src/main/java/com/example/cutetogether/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.example.cutetogether;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText mPassword;
     private Button mRegister;
     private Button mLogin;
+    private FirebaseAuth mAuth;
+    final String TAG = "ct";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         mEmail = findViewById(R.id.email_input_login);
         mPassword = findViewById(R.id.pwd_input_login);
         mRegister = findViewById(R.id.register_button);
@@ -37,7 +48,48 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ct", "Attempting login");
+                attemptLogin();
+            }
+        });
 
+
+
+    }
+
+    public void attemptLogin(){
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            sendToUserPage(user);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void sendToUserPage(FirebaseUser user){
+        Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+        finish();
+        startActivity(intent);
     }
 
 
---
+++ app/src/main/java/com/example/cutetogether/matchActivity.java
@@ -0,0 +1,15 @@
+package com.example.cutetogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class matchActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+    }
+}
